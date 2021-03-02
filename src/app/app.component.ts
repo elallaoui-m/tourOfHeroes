32f43097e4fe57d952a57ca5ecc9874dd8cce9d7
El Mehdi El Allaoui <emelallaoui@smb.sqli.com>
2021-03-02 09:42:24 +0100

    init heroes component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'hero-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-tour-of-heroes';
+  title = 'Tour of Heroes';
+
 }
