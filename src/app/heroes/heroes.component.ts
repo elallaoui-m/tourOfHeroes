8fb60cda74b35751fe96e21443fb4f5eed6099a3
El Mehdi El Allaoui <emelallaoui@SMB.SQLI.COM>
2021-03-02 12:26:45 +0100

    services

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, Inject, OnInit } from "@angular/core";
+import { HeroService } from "../hero.service";
+import { MessageService } from "../message.service";
 import { Hero } from "../models/hero";
 import { HEROES } from '../models/mock-heroes';
 
@@ -11,21 +13,31 @@ import { HEROES } from '../models/mock-heroes';
 })
 export class HeroesComponent implements  OnInit{
 
-    hero: Hero = {
-        id: 1,
-        name:  'Windstorm'
-    }
 
     selectedHero: Hero ;
+    heroes: Hero[]=[];
+    
+   constructor(private heroService: HeroService, private messageService: MessageService){
+   }
+
+       
+   ngOnInit (): void {
+        this.getHeroes();
+    }
+
+   getHeroes = (): void  => {
+         this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes);
+        //this.heroes  = HEROES;
+    }
+
+
 
     onSelect = (selectedHero: Hero) =>{
-        this.selectedHero = selectedHero
+        this.selectedHero = selectedHero;
+        this.messageService.add(`HeroesComponent: Selected hero id=${selectedHero.id}`);
     }
 
-    heroes = HEROES;
-    ngOnInit(): void {
-        
-    }
+
 
 
 
